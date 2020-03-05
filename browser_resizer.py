bd0e85305ffca83a09f3e3d4f2c1c6c063864e3c
AnKingMed <57678116+AnKingMed@users.noreply.github.com>
2020-03-05 08:52:18 -0700

    shrink editor update

---
+++ browser_resizer.py
@@ -0,0 +1,35 @@
+# Modified by: The AnKing 
+### Website: https://www.ankingmed.com  (Includes 40+ recommended add-ons)
+### Youtube: https://www.youtube.com/theanking
+### Instagram/Facebook: @ankingmed
+### Patreon: https://www.patreon.com/ankingmed (Get individualized help)
+
+from anki.hooks import wrap
+from aqt.browser import Browser
+from aqt import mw
+
+# Use Config to customize
+config = mw.addonManager.getConfig(__name__)
+
+def setupSidebar_wrapper(self, *_):
+    '''Reduce margins between items in Browser Sidebar'''
+    sidebar_margin = config.get("sidebar_margin", None)
+    if not sidebar_margin:
+        return
+    style = f"QTreeView::item {{margin: -{sidebar_margin}px;}}"
+    self.sidebarTree.setStyleSheet(style)
+
+Browser.setupSidebar = wrap(Browser.setupSidebar, setupSidebar_wrapper)
+
+def updateFont_wrapper(self, *_):
+    '''Reduce row height in Browser table view'''
+    reduce_row_height_by = config.get("reduce_row_height_by", None)
+    if not reduce_row_height_by:
+        return
+    vh = self.form.tableView.verticalHeader()
+    original_height = vh.defaultSectionSize()
+    new_height = original_height - reduce_row_height_by
+    vh.setMinimumSectionSize(new_height)
+    vh.setDefaultSectionSize(new_height)
+
+Browser.updateFont = wrap(Browser.updateFont, updateFont_wrapper, 'after')    
--- config.md
+++ config.md
@@ -2,8 +2,9 @@
 
 These settings do not require a restart to apply.
 
+- **editor_shrink** _(true or false)_ Shrink the fields in the editor to make things more compact. This feature only works in Anki 2.1.21+ and requires a restart to apply.
 - **reduce_row_height_by** Changes the row height of cards listed in the main browser screen. A higher number is a smaller margin. Default: 2
-- **sidebar_margin**: Changes the margin between items on the sidebar in the browser. A higher number is a smaller margin. Default: 0.8
+- **sidebar_margin**: Changes the margin between items on the sidebar in the browser. A higher number is a smaller margin. Default: 1 (must be increments of .5)
 
 (all decimals must be formatted #.#)
 
