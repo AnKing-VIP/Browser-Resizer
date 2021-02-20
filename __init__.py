497e35ef1c391fa1313d130d7a5b21cb07aefe75
ijgnd <does_not_exist@example.com>
2021-02-20 01:43:57 +0100

    move copyright notice to init

--- __init__.py
+++ __init__.py
@@ -1,3 +1,16 @@
+# Copyright: ijgnd
+#            Ankitects Pty Ltd and contributors
+# Modified by: The AnKing 
+#
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html 
+
+
+### Website: https://www.ankingmed.com  (Includes 40+ recommended add-ons)
+### Youtube: https://www.youtube.com/theanking
+### Instagram/Facebook: @ankingmed
+### Patreon: https://www.patreon.com/ankingmed (Get individualized help)
+
+
 from .config import anki_point_version, gc
 
 
--- shrink_editor.py
+++ shrink_editor.py
@@ -1,17 +1,3 @@
-# Copyright: ijgnd
-#            Ankitects Pty Ltd and contributors
-# Modified by: The AnKing 
-#
-# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html 
-
-
-### Website: https://www.ankingmed.com  (Includes 40+ recommended add-ons)
-### Youtube: https://www.youtube.com/theanking
-### Instagram/Facebook: @ankingmed
-### Patreon: https://www.patreon.com/ankingmed (Get individualized help)
-
-
-
 # This is a simple solution without the new style hooks for basic changes of the style in the editor:
 # from aqt import editor
 # editor_style = """
